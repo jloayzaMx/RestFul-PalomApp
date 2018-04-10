041c1eb6e10a1cd787fbfd6fc9d38378014bb04e
jloayzaMx <jose_18_06@hotmail.com>
2018-04-10 11:06:43 -0500

    heroku - pg

--- src/models/administrador.js
+++ src/models/administrador.js
@@ -1,15 +1,17 @@
-const mysql = require('mysql');
-connection = mysql.createConnection({
-host: 'sedipro-ucss.com',
-user: 'tawwwbus@localhost',
-password:'',
-database: 'tawwwbus_sedipro_palomapp'
+const Pool = require('pg-pool');
+const pool = new Pool({
+host: 'ec2-54-243-213-188.compute-1.amazonaws.com',
+port: '5432',
+user: 'yqghebejkgtyuk',
+password:'e4ec888ff29da9d31eeb4111990c441462a95d4087f8cb6230151767a51712b2',
+database: 'dfip05gaio33q9',
+ssl: true
 });
 
 let AdministradorModel ={}
 AdministradorModel.getAdministrador=(callback)=>{
-  if (connection) {
-    connection.query('select * from administrador order by id_administrador',
+  if (pool) {
+    pool.query('select * from administrador order by id_administrador',
     (err,rows)=>{
       if (err) {
         throw err;
@@ -23,8 +25,8 @@ AdministradorModel.getAdministrador=(callback)=>{
   }
 };
 AdministradorModel.insertAdministrador=(administradorData,callback)=>{
-if (connection) {
-  connection.query(
+if (pool) {
+  pool.query(
     'insert into administrador set ?',administradorData,
 (err,results)=>{
   if (err) {
--- src/models/local.js
+++ src/models/local.js
@@ -1,15 +1,17 @@
-const mysql = require('mysql');
-connection = mysql.createConnection({
-host: 'localhost',
-user: 'root',
-password:'',
-database: 'sedipro_palomapp'
+const Pool = require('pg-pool');
+const pool = new Pool({
+host: 'ec2-54-243-213-188.compute-1.amazonaws.com',
+port: '5432',
+user: 'yqghebejkgtyuk',
+password:'e4ec888ff29da9d31eeb4111990c441462a95d4087f8cb6230151767a51712b2',
+database: 'dfip05gaio33q9',
+ssl: true
 });
 
 let localModel ={}
 localModel.getLocal=(callback)=>{
-  if (connection) {
-    connection.query('select * from ....',
+  if (pool) {
+    pool.query('select * from ....',
     (err,rows)=>{
       if (err) {
         throw err;
